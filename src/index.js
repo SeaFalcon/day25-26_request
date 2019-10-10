bd3aba9101753f0bc73659942aa2f0bfecdaf450
SeaFalcon <seafalcon323@gmail.com>
2019-10-11 01:16:15 +0900

    1011Challenge

--- src/index.js
+++ src/index.js
@@ -6,18 +6,17 @@ const app = express();
 app.get("/", (req, res) => {
   let { url } = req.query;
   if (typeof url === "string" && !url.includes("http")) url = `http://${url}`;
-  console.log(url);
-  request(url, (err, res2, body) => {
-    console.log("error:", err); // Print the error if one occurred
-    console.log("statusCode:", res2 && res2.statusCode); // Print the response status code if a response was received
-    // console.log("body:", body); // Print the HTML for the Google homepage.
+  request(url, (err, res2) => {
+    if (err) {
+      console.log("error:", err);
+      return res.json("down");
+    }
 
-    if (res2.statusCode <= 445) return res.send("up");
-    return res.json("down");
+    if (res2.statusCode && res2.statusCode <= 445) return res.send("up");
   });
 });
 
 // Codesanbox does not need PORT :)
-app.listen(() => console.log(`Listening!`));
+app.listen(4000, () => console.log(`Listening!`));
 
 // /?url=nomadcoders.co
